02ba909010a73edee5946ef5b7a04520dc9e0b75
Hadi T <hadi@syniol.com>
2025-08-10 19:52:42 +0100

    Added enrichment service

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,9 +1,31 @@
 import { ClientConfig } from './config'
+import {
+  Enrichment,
+  EnrichmentCollectionStatus,
+  EnrichmentRequest,
+  EnrichmentResponse,
+  EnrichTransactionCollectionResponse,
+} from './enrichment'
 
-export class Client {
-  public constructor(private clientConfig: ClientConfig) {}
+export class Client implements Enrichment {
+  public constructor(
+    private readonly clientConfig: ClientConfig,
+  ) {
+  }
 
   public health(): Error | undefined {
+    return undefined
+  }
+
+  public enrichTransaction(request: EnrichmentRequest): EnrichmentResponse {
+    return undefined
+  }
+
+  public enrichTransactionCollection(request: EnrichmentRequest[]): EnrichTransactionCollectionResponse {
+    return undefined
+  }
+
+  public enrichTransactionCollectionStatus(id: string): EnrichmentCollectionStatus {
     return undefined
   }
 }
---
+++ src/client/enrichment.ts
@@ -0,0 +1,29 @@
+
+export interface EnrichmentRequest {
+  content:     string
+  countryCode: string
+}
+
+export interface EnrichmentResponse {
+  merchant:    string,
+  description: string,
+  categories:  string[],
+  logo:       string,
+}
+
+export interface EnrichTransactionCollectionResponse  {
+  id:   string
+  link: string
+}
+
+export enum EnrichmentCollectionStatus {
+  EnrichmentCollectionStatusReady   = "READY",
+  EnrichmentCollectionStatusFailure = "FAILED",
+  EnrichmentCollectionStatusPending = "PENDING",
+}
+
+export interface Enrichment {
+  enrichTransaction(request: EnrichmentRequest): EnrichmentResponse
+  enrichTransactionCollection(request: EnrichmentRequest[]): EnrichTransactionCollectionResponse
+  enrichTransactionCollectionStatus(id: string): EnrichmentCollectionStatus
+}
