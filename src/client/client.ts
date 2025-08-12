4cdc03a409acb89861f314cf808913095ca177be
Hadi T <hadi@syniol.com>
2025-08-12 06:59:15 +0100

    new release

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,19 +1,19 @@
 import { ClientConfig } from './config'
-import {
-  Enrichment,
-  EnrichmentCollectionStatus,
-  EnrichmentRequest,
-  EnrichmentResponse,
-  EnrichTransactionCollectionResponse,
-} from './enrichment'
+
+import { HttpStatusCode, Request } from 'carbon-http'
 
 export class Client {
+  private readonly httpRequest: <Opt, Resp>(ur: string, opt: Opt) => Promise<Resp>
+
   public constructor(
     private readonly clientConfig: ClientConfig,
   ) {
+    this.httpRequest = Request
   }
 
-  public health(): Error | undefined {
-    return undefined
+  public async health(): Promise<boolean> {
+    const res = await Request('https://api.xyo.financial/healthz')
+
+    return res.status === HttpStatusCode.OK
   }
 }
