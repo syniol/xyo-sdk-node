c017acc5540deaa001626e8208c20c9eff112929
Hadi T <hadi@syniol.com>
2025-08-12 07:03:18 +0100

    removed dist

--- dist/client/client.d.ts
+++
@@ -1,8 +0,0 @@
-import { ClientConfig } from './config';
-export declare class Client {
-    private readonly clientConfig;
-    private readonly httpRequest;
-    constructor(clientConfig: ClientConfig);
-    health(): Promise<boolean>;
-}
-//# sourceMappingURL=client.d.ts.map
