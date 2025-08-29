487ad149a83b42796a2080ba802c526775a40d05
Hadi T <hadi@syniol.com>
2025-08-29 21:42:11 +0100

    changes I dont remember

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,10 +1,16 @@
-import { HttpStatusCode, Request, CarbonHttpRequestOption } from 'carbon-http'
+import { CarbonHttpRequestOption, CarbonHttpResponse, HttpMethod, HttpStatusCode, Request } from 'carbon-http'
 
 import { ClientConfig } from './config'
-import { CarbonHttpResponse } from 'carbon-http/dist/types/http/type'
+import {
+  Enrichment,
+  EnrichmentCollectionStatus,
+  EnrichmentRequest,
+  EnrichmentResponse,
+  EnrichTransactionCollectionResponse, EnrichTransactionCollectionStatusResponse,
+} from './enrichment'
 
 
-export class Client {
+export class Client implements Enrichment {
   private readonly httpRequest: <T>(ur: string, opt: CarbonHttpRequestOption) => Promise<CarbonHttpResponse<T>>
 
   public constructor(
@@ -17,5 +23,63 @@ export class Client {
     const res = await Request('https://api.xyo.financial/healthz')
 
     return res.status === HttpStatusCode.OK
+  }
+
+  public async enrichTransaction(request: EnrichmentRequest): Promise<EnrichmentResponse> {
+    const resp = await Request<EnrichmentResponse>(
+      'https://api.xyo.financial/v1/ai/finance/enrichment/transaction',
+      {
+        method: HttpMethod.POST,
+        headers: this.requiredHeaders,
+        body: JSON.stringify(request),
+      }
+    )
+
+    if (resp.status !== HttpStatusCode.OK) {
+      throw new Error('')
+    }
+
+    return resp.json()
+  }
+
+  public async enrichTransactionCollection(request: EnrichmentRequest[]): Promise<EnrichTransactionCollectionResponse> {
+    const resp = await Request<EnrichTransactionCollectionResponse>(
+      'https://api.xyo.financial/v1/ai/finance/enrichment/transactions',
+      {
+        method: HttpMethod.POST,
+        headers: this.requiredHeaders,
+        body: JSON.stringify(request),
+      }
+    )
+
+    if (resp.status !== HttpStatusCode.OK) {
+      throw new Error('')
+    }
+
+    return resp.json()
+  }
+
+  public async enrichTransactionCollectionStatus(id: string): Promise<EnrichmentCollectionStatus> {
+    const resp = await Request<EnrichTransactionCollectionStatusResponse>(
+      `https://api.xyo.financial/v1/ai/finance/enrichment/transactions/status/${id}`,
+      {
+        method: HttpMethod.GET,
+        headers: this.requiredHeaders,
+      }
+    )
+
+    if (resp.status !== HttpStatusCode.OK) {
+      throw new Error('')
+    }
+
+    return resp.json().status
+  }
+
+  private get requiredHeaders(): Record<string, string> {
+    return {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${this.clientConfig.APIKey}`
+    }
   }
 }
