4a2915dbbc23b01ae69b9c726704c6b06c8a920a
Hadi T <hadi@syniol.com>
2025-09-01 13:18:50 +0100

    resolved styling issue with test

--- src/enrichment/service.test.ts
+++ src/enrichment/service.test.ts
@@ -1,11 +1,13 @@
-import { describe, test } from 'node:test'
+import { suite, test } from 'node:test'
 
-describe('Enrichment Test', () => {
-  test('It should do positive', (t) => {
-    t.assert.equal('1', '1')
-  })
+;(async () => {
+  await suite('Enrichment Test', async () => {
+    await test('It should do positive', (t) => {
+      t.assert.equal('1', '1')
+    })
 
-  test('It should do negative', (t) => {
-    t.assert.notEqual('2', '1')
+    await test('It should do negative', (t) => {
+      t.assert.notEqual('2', '1')
+    })
   })
-})
+})()
