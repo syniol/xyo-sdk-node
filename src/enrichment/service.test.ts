02dc09999f0fc20e3f3c6320fd01c6eef822adda
Hadi T <hadi@syniol.com>
2025-09-01 21:40:18 +0100

    added todo for final test collection status

--- src/enrichment/service.test.ts
+++ src/enrichment/service.test.ts
@@ -1,4 +1,4 @@
-import { describe, suite, test } from 'node:test'
+import { describe, suite, test, todo } from 'node:test'
 import {
   CarbonHttpResponse,
   HttpStatusCode,
@@ -210,5 +210,112 @@ void (async () => {
         }
       })
     })
+
+    await todo(
+      'Test enrichTransactionCollection',
+      async () => {
+        await test('when status code is 200', async (t) => {
+          const mockedEnrichmentResponse: EnrichTransactionCollectionResponse =
+            {
+              id: '411f991f-2b62-4dc9-aaa8-13bf3610152a',
+              link: 'ftp://storage.xyo.financial/enrichment/download/411f991f-2b62-4dc9-aaa8-13bf3610152a.tar.gz',
+            }
+
+          const mockedClientConfig: ClientConfig = {
+            options: {
+              apiKey:
+                'YourApiKeyFromXYO.FinancialDashboard',
+            },
+            get requiredHeaders(): Record<string, string> {
+              return {
+                Authorization: `Bearer ${this.options.apiKey}`,
+              }
+            },
+            httpRequest: (): Promise<
+              CarbonHttpResponse<EnrichTransactionCollectionResponse>
+            > => {
+              const mockedHttpRequestResponse = {
+                status: HttpStatusCode.OK,
+                text(): string {
+                  return JSON.stringify(
+                    mockedEnrichmentResponse,
+                  )
+                },
+                json(): EnrichTransactionCollectionResponse {
+                  return mockedEnrichmentResponse
+                },
+              } as CarbonHttpResponse<EnrichTransactionCollectionResponse>
+
+              return Promise.resolve(
+                mockedHttpRequestResponse,
+              )
+            },
+          }
+
+          const sut = new EnrichmentService(
+            mockedClientConfig,
+          )
+          const actual = await sut.enrichTransaction({
+            content: 'Syniol Software Consultancy',
+            countryCode: 'GB',
+          })
+
+          t.assert.deepEqual(
+            actual,
+            mockedEnrichmentResponse,
+          )
+        })
+
+        await test('when status code is not 200', async (t) => {
+          const mockedClientConfig: ClientConfig = {
+            options: {
+              apiKey:
+                'YourApiKeyFromXYO.FinancialDashboard',
+            },
+            get requiredHeaders(): Record<string, string> {
+              return {
+                Authorization: `Bearer ${this.options.apiKey}`,
+              }
+            },
+            httpRequest: (): Promise<
+              CarbonHttpResponse<EnrichTransactionCollectionResponse>
+            > => {
+              const mockedHttpRequestResponse = {
+                status: HttpStatusCode.BAD_REQUEST,
+                text(): string {
+                  return 'error with the request'
+                },
+              } as CarbonHttpResponse<EnrichTransactionCollectionResponse>
+
+              return Promise.resolve(
+                mockedHttpRequestResponse,
+              )
+            },
+          }
+
+          const sut = new EnrichmentService(
+            mockedClientConfig,
+          )
+
+          try {
+            await sut.enrichTransactionCollection([
+              {
+                content: 'Syniol Software Consultancy',
+                countryCode: 'GB',
+              },
+            ])
+          } catch (e) {
+            t.assert.equal(
+              e.statusCode,
+              HttpStatusCode.BAD_REQUEST,
+            )
+            t.assert.equal(
+              e.message,
+              'error with the request',
+            )
+          }
+        })
+      },
+    )
   })
 })()
